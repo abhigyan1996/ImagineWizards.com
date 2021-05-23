6b93ef38333c7ccd96ad04d72206ce83b8087393
Abhigyan Kashyap <abhigyan.kashyap@pinelabs.com>
2021-05-23 12:53:07 +0530

    oto done

--- config/config.js
+++ config/config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    email:"abhigyankashyap82@gmail.com",
-    password:"AbhiQuarkX321#"
-  };
+    email:"quarkx.subscription@gmail.com",
+    clientId:'116629966069-ms8306u5nrjesrho94skhaphl11hf0nh.apps.googleusercontent.com',
+    clientSecret:'0PUuTy-CwSYWW4Z6IFw9_cPe',
+    refreshToken:'1//04Qy8-1_EXPrtCgYIARAAGAQSNwF-L9IrC4_xQOVv9Alj10qwsP2GBfGCFDpOTT7UAmcplhuwKeUMhapqFqhh_tvniKsMhI37uQ8',
+    redirectUri:'https://developers.google.com/oauthplayground'
+ };
   
--- utils.js
+++ utils.js
@@ -4,7 +4,14 @@ var config=require("./config/config");
 var otpGenerator = require('otp-generator');
 const NodeCache = require( "node-cache" );
 var bcrypt=require("bcrypt");
+var {google}= require("googleapis");
 const myCache = new NodeCache( { stdTTL: 3 * 60, checkperiod: 100} );
+var transporter=null;
+
+const oAuthClient=new google.auth.OAuth2(config.clientId,config.clientSecret,config.redirectUri);
+oAuthClient.setCredentials({refresh_token:config.refreshToken});
+
+
 
 const passport = require("passport"); //This is used for authentication
 const LocalStrategy = require("passport-local").Strategy;
@@ -47,17 +54,31 @@ passport.deserializeUser(function(id, done) {
   });          
 });
 
-var transporter = nodemailer.createTransport(smtpTransport({
+
+var createTransporter=async function()
+{
+
+let AccessToken=await oAuthClient.getAccessToken(); 
+transporter = nodemailer.createTransport({
   service: 'gmail',
-  host: 'smtp.gmail.com',
   auth: {
+    type:'OAuth2',
     user: config.email,
-    pass: config.password
+    clientId:config.clientId,
+    clientSecret:config.clientSecret,
+    refreshToken:config.refreshToken,
+    accessToken:AccessToken
   }
-}));
+});
+
+
+
+
+}
 
 var SendOtp=async function (Sendtoemail,strpurpose)
 {
+  
   let otp=otpGenerator.generate(6, { upperCase: false, specialChars: false ,alphabets:false});
   myCache.set( Sendtoemail, otp)
   var mailOptions = {
@@ -68,6 +89,7 @@ var SendOtp=async function (Sendtoemail,strpurpose)
    };
   try
   {
+    await createTransporter();
     let res=await transporter.sendMail(mailOptions);
     return true;
   }
